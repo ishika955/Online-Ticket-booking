c9e6a651dadb9e8c87c9d1f4e0aabf49fcc4e882
Muskan <muskanmittal249@gmail.com>
2025-08-26 13:27:59 +0530

    Making login page

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 // form-section;
 let flightSection=document.getElementById("flights-section");
 let busSection=document.getElementById("buses-section");
-let cabSection=document.getElementById("cab-section");
-let trainSection=document.getElementById("train-section");
-let hotelSection=document.getElementById("hotel-section");
+let cabSection=document.getElementById("cabs-section");
+let trainSection=document.getElementById("trains-section");
+let hotelSection=document.getElementById("hotels-section");
 
 let flight=document.getElementById("flight");
 let bus=document.getElementById("bus");
@@ -11,59 +11,46 @@ let cab=document.getElementById("cab");
 let train=document.getElementById("train");
 let hotel=document.getElementById("hotel");
 
-flightSection.style.visibility="visible";
-busSection.style.visibility="hidden";
-cabSection.style.visibility="hidden";
-trainSection.style.visibility="hidden";
-hotelSection.style.visibility="hidden";
+ flightSection.style.display=" block";
+busSection.style.display="none";
+cabSection.style.display="none";
+trainSection.style.display="none";
+hotelSection.style.display="none";
 
 function navbar(){
    if(flight.checked==true){
-   flightSection.style.visibility="visible";
-busSection.style.visibility="hidden";
-cabSection.style.visibility="hidden";
-trainSection.style.visibility="hidden";
-hotelSection.style.visibility="hidden";
+   flightSection.style.display=" block";
+busSection.style.display="none";
+cabSection.style.display="none";
+trainSection.style.display="none";
+hotelSection.style.display="none";
    }
    if(bus.checked==true){
-    flightSection.style.visibility="visible";
-busSection.style.visibility="hidden";
-cabSection.style.visibility="hidden";
-trainSection.style.visibility="hidden";
-hotelSection.style.visibility="hidden";
+    flightSection.style.display="none";
+busSection.style.display="block";
+cabSection.style.display="none";
+trainSection.style.display="none";
+hotelSection.style.display="none";
    }
     if(train.checked==true){
-  flightSection.style.visibility="visible";
-busSection.style.visibility="hidden";
-cabSection.style.visibility="hidden";
-trainSection.style.visibility="hidden";
-hotelSection.style.visibility="hidden";
+ flightSection.style.display="none";
+busSection.style.display="none";
+cabSection.style.display="none";
+trainSection.style.display="block";
+hotelSection.style.display="none";
    }
     if(hotel.checked==true){
-   flightSection.style.visibility="visible";
-busSection.style.visibility="hidden";
-cabSection.style.visibility="hidden";
-trainSection.style.visibility="hidden";
-hotelSection.style.visibility="hidden";
+  flightSection.style.display=" none";
+busSection.style.display="none";
+cabSection.style.display="none";
+trainSection.style.display="none";
+hotelSection.style.display="block";
    }
    if(cab.checked==true){
-  flightSection.style.visibility="visible";
-busSection.style.visibility="hidden";
-cabSection.style.visibility="hidden";
-trainSection.style.visibility="hidden";
-hotelSection.style.visibility="hidden";
+ flightSection.style.display=" none";
+busSection.style.display="none";
+cabSection.style.display="block";
+trainSection.style.display="none";
+hotelSection.style.display="none";
    }
-
-
-   window.onload=function(){
-    flight.checked=true;
-    navbar();
-   }
-   
-   
-
-   
-
-
-
 }
